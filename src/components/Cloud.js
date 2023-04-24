9b45ad42e5fab17237b131c027c44f5994a9419d
TomerAsulin <tomermailg@gmail.com>
2023-04-24 12:13:05 +0300

    add quotes

--- src/components/Cloud.js
+++ src/components/Cloud.js
@@ -2,6 +2,8 @@ import { useState } from 'react';
 
 const quotes = [
   `"Security is always excessive until it's not enough." – Robbie Sinclair`,
+  `"One of my most productive days was throwing away 1,000 lines of code." - Ken Thompson `,
+  `"Any app that can be written in JavaScript will eventually be written in JavaScript." – Atwood's Law`,
 ];
 
 const Cloud = () => {
